65a8d4cd3ec294b342fc3701e4bd0c06a248db71
Zeni <Zeni@WIN-THVJGG0E067>
2024-03-22 02:41:40 +0300

    Add project files.

---
+++ MoonLoader/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MoonLoader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 360;
+            da.RepeatBehavior = RepeatBehavior.Forever;
+            da.Duration = new Duration(TimeSpan.FromSeconds(5));
+
+            RotateTransform rt = new RotateTransform();
+            moon.RenderTransformOrigin = new Point(2.5, 2.5);
+            moon.RenderTransform = rt;
+
+            rt.BeginAnimation(RotateTransform.AngleProperty, da);
+        }
+      
+    }
+}
